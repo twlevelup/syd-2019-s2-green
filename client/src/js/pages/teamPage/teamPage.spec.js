b52b8ca1c617e1cb043e0800a7065694d3fb139b
IzzyKohout <izzykohout@gmail.com>
2019-09-10 10:23:56 +0000

    added names to teampage

--- client/src/js/pages/teamPage/teamPage.spec.js
+++ client/src/js/pages/teamPage/teamPage.spec.js
@@ -16,8 +16,11 @@ describe('TeamPage', () => {
     it('should contain team members names', () => {
       const page = new TeamPage();
       expect(page.render()).toContain("Ray");
-      expect(page.render()).toContain("Daniel");
-      expect(page.render()).toContain("Sarang Love");
+
+      expect(page.render()).toContain("Matthew");
+      expect(page.render()).toContain("Isabelle");
+      expect(page.render()).toContain("Ellie");
+
     });
   });
 
