a8e7c301aa2aa311edcd45dc4fc4f07c33c9bdbf
EllieCreech <ellie.creech95@hotmail.com>
2019-09-24 10:45:56 +0000

    Current work with medium products.

---
+++ client/src/js/pages/Product/medium.js
@@ -0,0 +1,7 @@
+const BasePage = require('watch-framework').BasePage;
+
+class MediumPage extends BasePage {
+  template = require('./medium.hbs');
+}
+
+module.exports = MediumPage;
--- client/src/js/routes.js
+++ client/src/js/routes.js
@@ -2,10 +2,13 @@ const HomePage = require('./pages/homePage/homePage');
 const ContactsPage = require('./pages/contactsPage/contactsPage');
 const TeamPage = require('./pages/teamPage/teamPage');
 const FourOhFour = require('./pages/404Page/404Page');
-
+const medium = require("./pages/Product/medium");
+// const mediumProducts = require("./pages/Product/")
 module.exports = {
   '/': HomePage,
   'contacts': ContactsPage,
   'team': TeamPage,
   '404': FourOhFour,
+  "medium": medium,
+  // "medium_products": mediumProducts
 };
