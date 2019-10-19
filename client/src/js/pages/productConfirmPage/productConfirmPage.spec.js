4f1c272ccdbb49e4cefb485e9f991fd9f3993b47
IzzyKohout <izzykohout@gmail.com>
2019-10-19 01:05:47 +0000

    <IK> Formatted productConfirmPage

--- client/src/js/pages/productConfirmPage/productConfirmPage.spec.js
+++ client/src/js/pages/productConfirmPage/productConfirmPage.spec.js
@@ -1,1 +1,21 @@
 
+const HomePage = require('./homePage');
+const ProductConfirmPage  =require('./productConfirmPage');
+const StorageHub = require('watch-framework').StorageHub;
+
+describe('productConfirmPage', () => {
+  let watchFace;
+  beforeEach(() => {
+    document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
+    watchFace = document.getElementById('watch-face');
+  });
+
+  describe('#bottomButtonEvent', () => {
+    it('should redirect to home page when you click bottom button on product confirm page', () => {
+      const page = new ProductConfirmPage();
+      spyOn(page, 'navigate');
+
+      page.bottomButtonEvent();
+      expect(page.navigate).toHaveBeenCalledWith('/');
+    });
+  });
