28ea48d2aec7ef91d201f8ea5d025873c63791ac
IzzyKohout <izzykohout@gmail.com>
2019-10-19 00:04:40 +0000

    <IK> buy button takes you to product confirm page

---
+++ client/src/js/pages/productConfirmPage/productConfirmPage.js
@@ -0,0 +1,8 @@
+const BasePage = require('watch-framework').BasePage;
+
+class ProductConfirmPage extends BasePage {
+  template = require('./productConfirmPage.hbs');
+
+}
+
+module.exports = ProductConfirmPage;
